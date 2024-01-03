fc0a3a13d8b4b8988847c8dcbe4f8422bbc99ad3
Raju Dasa <36834333+RajuDasa@users.noreply.github.com>
2024-01-03 23:37:29 +0530

    updated client with better wording

--- unit-testing-using-dotnet-test/PrimeService.Client/Program.cs
+++ unit-testing-using-dotnet-test/PrimeService.Client/Program.cs
@@ -3,4 +3,4 @@
 //uses implict using
 // See https://aka.ms/new-console-template for more information
 var obj = new PrimeService.PrimeService();
-Console.WriteLine("is 1 prime: " + obj.IsPrime(1));
+Console.WriteLine("Is 1 Prime number? : " + obj.IsPrime(1));
