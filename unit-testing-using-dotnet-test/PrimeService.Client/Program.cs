8a414f76ebb8925c0840fb9010c38efe3ba69855
Raju Dasa <36834333+RajuDasa@users.noreply.github.com>
2024-01-11 11:01:02 +0530

    arg passed by cmd line

--- unit-testing-using-dotnet-test/PrimeService.Client/Program.cs
+++ unit-testing-using-dotnet-test/PrimeService.Client/Program.cs
@@ -3,18 +3,24 @@
 //uses implict using
 // See https://aka.ms/new-console-template for more information
 
-var obj = new PrimeService.PrimeService();
-int number;
-
-Console.WriteLine("** Prime number checker **");
-Console.WriteLine("Enter int number: ");
-number = Convert.ToInt32(Console.ReadLine());
-
-if (obj.IsPrime(number))
+public static void main(string[] args)
 {
-    Console.WriteLine("It is prime");
-}
-else
-{
-    Console.WriteLine("It is not prime");
+    if(args == null || args.Length ==0)
+        throw new InvalidArgumentError();
+
+    var obj = new PrimeService.PrimeService();
+    int number;
+
+    Console.WriteLine("** Prime number checker **");
+    Console.WriteLine("passed int number: ${args[0]} ");
+    number = Convert.ToInt32(args[0]);
+
+    if (obj.IsPrime(number))
+    {
+        Console.WriteLine("It is prime");
+    }
+    else
+    {
+        Console.WriteLine("It is not prime");
+    }
 }
